2f13897706c8de0e9046baa1a6958d404d31c95c
AayushMallya <aayushmallya@outlook.com>
2026-07-18 14:09:06 -0400

    boilerplate

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("API_TOKEN")
+
+headers = {}
+
+def get_tendor():
+    #do stuff
+    return
+
+def post_tendor():
+    #do stuff again
+    return
+
+def rateLimit():
+    #rate limit the posts 
+    return
+
+def main():
+    #run stuff
+    print("test")
+
+main()
